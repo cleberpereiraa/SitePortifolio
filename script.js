9e2a427239b534a80b0c9db06b9040f30a4faaa0
Cleber da Silva Pereira <cleberpereira100@gmail.com>
2021-08-12 01:46:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function clicou(){
+     var p = document.getElementById("paragrafoEscondido")
+    p.style.visibility = "visible"
+}
+   
+function retirou(){
+    var p = document.getElementById("paragrafoEscondido")
+    p.style.visibility = "hidden"
+}
+
+
