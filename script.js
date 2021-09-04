27a8810e1b8c2ac248dd30c2ee2b7e79d7d29b3b
Cleber da Silva Pereira <cleberpereira100@gmail.com>
2021-09-04 00:37:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ function retirou(){
 
 
  */
+/***formulario */
+var submitButton = document.getElementById("submit_form");
+var form = document.getElementById("email_form");
+form.addEventListener("submit", function (e) {
+    setTimeout(function() {
+        submitButton.value = "Sending...";
+        submitButton.disabled = true;
+    }, 1);
+});
